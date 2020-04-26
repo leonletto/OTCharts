fe8da7222bb70f5aee9d8253e3657e2eb3eadcc6
zaradana <zara@zaradana.com>
2020-04-26 13:29:36 -0700

    port changed

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const chartRoutes = require('./routes/chartRoutes');
 const app = express();
 app.use(chartRoutes);
 
-const PORT = process.env.PORT || 1337;
+const port = process.env.PORT || 1337;
 
-app.listen(PORT, () => {
-    console.log('Listening on port '+ PORT);
+app.listen(port, () => {
+    console.log('Listening on port '+ port);
 });
