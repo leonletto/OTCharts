d2b1f9c87456fd6ea72ac78c8eb0caf1875847c0
zaradana <zara@zaradana.com>
2020-05-02 15:15:28 -0700

    auth is passed in the widget call

--- index.js
+++ index.js
@@ -12,97 +12,89 @@ app.use(bodyParser.urlencoded( { extended: true }));
 app.use(chartRoutes);
 
 const port = process.env.PORT || 1337;
-
-let bear = 'Hi';
-
-app.post('/api/reporting/v1/dashboard/widget/bear', async(req,res)=>{
-    const {bearer} = req.body;
-    if(!bearer){
-        res.status(422).send({error:'You must provide a bear'});
-    } else{
-      bear = bearer;
-      res.status(200).send("Ok");
-    }   
-  });
   
-  app.post('/api/access/v1/oauth/token', async(req,res0)=>{
-    const {username,password,domain} = req.body;
-    if(!username |  !password | !domain ){
-        res.status(422).send({error:'You must provide a username, password, and domain.'});
-    } else {
-      var options = {
-        'method': 'POST',
-        'hostname': domain + '.otprivacy.com',
-        'path': '/api/access/v1/oauth/token',
-        'headers': {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Cookie': '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
-        },
-        'maxRedirects': 20
-      };  
-      var req2 = https.request(options, function (res) {
-        var chunks = [];
-      
-        res.on("data", function (chunk) {
-          chunks.push(chunk);
-        });
-      
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          res0.send(body);
-          console.log(body.toString());
-        });
-      
-        res.on("error", function (error) {
-          console.error(error);
-          res0.send(error);
-        });
+app.post('/api/access/v1/oauth/token', async(req,res0)=>{
+  const {username,password,domain} = req.body;
+  if(!username |  !password | !domain ){
+      res.status(422).send({error:'You must provide a username, password, and domain.'});
+  } else {
+    var options = {
+      'method': 'POST',
+      'hostname': domain + '.otprivacy.com',
+      'path': '/api/access/v1/oauth/token',
+      'headers': {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
+      },
+      'maxRedirects': 20
+    };  
+    var req2 = https.request(options, function (res) {
+      var chunks = [];
+    
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
       });
-
-      var postData = qs.stringify({
-        'grant_type': 'password',
-        'scope': 'read',
-        'username': username,
-        'password': password,
-        'client_id': 'onetrust'
+    
+      res.on("end", function (chunk) {
+        var body = Buffer.concat(chunks);
+        res0.send(body);
+        console.log(body.toString());
       });
-      req2.write(postData);  
-      req2.end();    
-    }   
-  });
+    
+      res.on("error", function (error) {
+        console.error(error);
+        res0.send(error);
+      });
+    });
 
-app.get('/api/reporting/v1/dashboard/widget/:id', async(req, res0) => {
+    var postData = qs.stringify({
+      'grant_type': 'password',
+      'scope': 'read',
+      'username': username,
+      'password': password,
+      'client_id': 'onetrust'
+    });
+    req2.write(postData);  
+    req2.end();    
+  }   
+});
+
+app.get('/api/reporting/v1/dashboard/widget/:domain/:id', async(req, res0) => {
     route = req.params.id;
-    // const {bear} = req.header.Authorization; //final auth is going to use this
-    if(bear.length > 10)
-    {   
-        let options = {
-          'method': 'GET',
-          'hostname': 'demo1.otprivacy.com',
-          'path': '/api/reporting/v1/dashboard/widget/'+route,
-          'headers': {
-            'Authorization': 'Bearer '+ bear, 
-            'Cookie': '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
-          },
-          'maxRedirects': 20
-        }
-        https.request(options, function (res) {
-        var chunks = [];
-      
-        res.on("data", function (chunk) {
-          chunks.push(chunk);
-        });
-      
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          res0.send(body);
-        });
-      
-        res.on("error", function (error) {
-          console.error(error);
-        });
-      }).end();
+    domain = req.params.domain;
+    const {authorization} = req.headers;
+    if (!authorization) 
+    {
+        return res0.status(401).send({error: 'You are not authorized!'});
     }
+  
+    let options = {
+      'method': 'GET',
+      'hostname': domain+'.otprivacy.com',
+      'path': '/api/reporting/v1/dashboard/widget/'+route,
+      'headers': {
+        'Authorization': authorization, 
+        'Cookie': '__cfduid=deed201afd27e50d8dc45ea9a40b913f91587694655'
+      },
+      'maxRedirects': 20
+    }
+    https.request(options, function (res) {
+    var chunks = [];
+  
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+  
+    res.on("end", function (chunk) {
+      var body = Buffer.concat(chunks);
+      res0.send(body);
+    });
+  
+    res.on("error", function (error) {
+      console.error(error);
+    });
+  }).end();
+
  });
 
 app.use(express.static(path.join(__dirname, '/src/dist')));
